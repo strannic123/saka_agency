5be43831ec109988ee57dc436c0b59084fd34be6
strannic123 <manager350@mail.ru>
2022-02-18 14:32:49 +0300

    added burger + animation

--- js/main.js
+++ js/main.js
@@ -4,3 +4,16 @@ $(document).ready(function(){
         dots: true
 });
 });
+
+(function () {
+    const burger = document.querySelector('.header__burger')
+    const panelNavBurger = document.querySelector('.header__nav')
+    const close = document.querySelector('.header__nav-close')
+    burger.addEventListener('click', () => {
+        panelNavBurger.classList.add('header__nav-active')
+    })
+
+    close.addEventListener('click', () => {
+        panelNavBurger.classList.remove('header__nav-active')
+    })
+})()
